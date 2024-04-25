6bfdccc0f109a0670662dd2e750ab53a571ecec1
rslewenstein <rafasoaresjf@gmail.com>
2024-04-25 16:49:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Script to update Golang version
+
+Sometimes to update Golang is bored when we use Linux.
+Thinking about it, I created a script to automatize this process.
+
+- You need to download the go version
+```
+https://go.dev/dl/
+```
+
+- Giving permissions to script:
+```
+chmod u+x go-update.sh
+```
+
+- Execute the script:
+```
+./go-update.sh
+```
---
+++ go-update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd /home/rafael/Downloads
+pwd
+
+find . -iname go* | while read fname; do
+    echo "$fname";
+    tar -xvzf /home/rafael/Downloads/$fname
+done
+
+echo "Old version: "
+go version
+
+which go | while read sname; do
+    echo "$sname";
+    mkdir /home/rafael/Downloads/go-temp
+    sudo mv /usr/local/go /home/rafael/Downloads/go-temp
+    sudo mv /home/rafael/Downloads/go /usr/local/.
+done
+
+rm -r /home/rafael/Downloads/go-temp
+
+echo "New version: "
+go version
